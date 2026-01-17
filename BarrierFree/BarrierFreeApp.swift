673e2f882988172e06be10b5f9f90cef04d9193a
Kiyan <yildizkiyan@icloud.com>
2026-01-17 15:12:01 +0100

    Initial Commit

---
+++ BarrierFree/BarrierFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarrierFreeApp.swift
+//  BarrierFree
+//
+//  Created by TA616 on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct BarrierFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarrierFree/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarrierFree
+//
+//  Created by TA616 on 17.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
